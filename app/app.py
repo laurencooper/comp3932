d3b16c0414108e672cfe92c603306b85d1337b80
Lauren <sc18lrec@leeds.ac.uk>
2022-04-12 22:28:56 +0100

    initial commit

---
+++ app/app.py
@@ -0,0 +1,30 @@
+from flask import Flask, jsonify
+
+app= Flask(__name__)
+
+
+#https://www.geeksforgeeks.org/calculate-pi-with-python/ 
+
+
+@app.route("/")
+def calculate_pi():
+     # Initialize denominator
+    k = 1
+
+    # Initialize sum
+    s = 0
+
+    for i in range(100000000):
+	    # even index elements are positive
+	    if i % 2 == 0:
+		    s += 4/k
+	    else:
+		    # odd index elements are negative
+		    s -= 4/k
+	    # denominator is odd
+	    k += 2
+	
+    return jsonify({"pi": str(s)})
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ molotov/molotov.py
@@ -0,0 +1,16 @@
+"""
+
+This Molotov script has 2 scenario
+
+"""
+from molotov import scenario
+
+
+_API = "http://34.105.139.200"
+
+
+
+@scenario(weight=100)
+async def scenario_two(session):
+    async with session.get(_API) as resp:
+        assert resp.status == 200, resp.status
